6bd8a83890a790083412d06f4aa213abec3ebb0c
Romchansky <romkapetrenko8@gmail.com>
2021-01-14 21:23:05 +0200

    first Ex.1

---
+++ src/main/java/com/library/Publication.java
@@ -0,0 +1,15 @@
+package com.library;
+
+public class Publication {
+    private String name;
+    private int countPages;
+
+    public Publication(String name, int countPages) {
+        this.name = name;
+        this.countPages = countPages;
+    }
+
+    public String print() {
+        return "name =" + this.name + ", count pages" + this.countPages;
+    }
+}
