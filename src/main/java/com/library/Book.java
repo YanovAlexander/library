980527e97c387f2e4caba1b5e9dfdf21f562f42c
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-01-14 20:41:03 +0100

    Added Publication Added Book

---
+++ src/main/java/com/library/Book.java
@@ -0,0 +1,13 @@
+package com.library;
+
+public class Book extends Publication{
+    private String author;
+    public Book(String name, int countPages, String author) {
+        super(name, countPages);
+        this.author = author;
+    }
+    @Override
+    public String print() {
+        return super.print() + ",author= " + this.author;
+    }
+}
