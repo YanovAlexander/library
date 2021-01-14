0d8cafc096efcb054a47c88388c82d6a6753f721
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2021-01-14 21:43:23 +0200

    added Publication and Book

---
+++ src/main/java/com/library/Book.java
@@ -0,0 +1,15 @@
+package com.library;
+
+public class Book extends Publication{
+    private String author;
+
+    public Book(String name, int countPages, String author) {
+        super(name, countPages);
+        this.author = author;
+    }
+
+    @Override
+    public String print(){
+     return "Book{" + super.print() + "author=" + this.author + "}";
+    }
+}
