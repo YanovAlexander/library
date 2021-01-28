aef97b54cca0ccadefd283b8e59d593b86ad7c58
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2021-01-28 21:31:50 +0200

    Ex.8 Add Console view

--- src/main/java/com/library/Book.java
+++ src/main/java/com/library/Book.java
@@ -1,5 +1,7 @@
 package com.library;
 
+import java.util.Objects;
+
 public class Book extends Publication {
     private String author;
 
@@ -13,5 +15,17 @@ public class Book extends Publication {
         return "Book{" + super.print() + ", author= " + this.author + "}";
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        if (!super.equals(o)) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(author);
+    }
 }
--- src/main/java/com/library/Journal.java
+++ src/main/java/com/library/Journal.java
@@ -1,5 +1,7 @@
 package com.library;
 
+import java.util.Objects;
+
 public class Journal extends Publication {
     private int number;
     private int publicationYear;
@@ -13,5 +15,19 @@ public class Journal extends Publication {
     @Override
     public String print() {
         return "Journal{" + super.print() + ", number=" + this.number + ", year=" + this.publicationYear + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Journal)) return false;
+        if (!super.equals(o)) return false;
+        Journal journal = (Journal) o;
+        return number == journal.number && publicationYear == journal.publicationYear;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, publicationYear);
     }
 }
