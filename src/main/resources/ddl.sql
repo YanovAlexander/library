f6f71fd573a5881c1d18f4fa3fd1885e311842c8
DenisAndrieiew <eretarmineret@gmail.com>
2021-05-19 21:38:07 +0300

    create book table & journal table to DDL

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,1 +1,19 @@
 create database library_go_it with owner postgres;
+
+create table book (
+id serial primary key,
+name varchar(250) not null,
+countPages int not null,
+publicationYear int not null,
+author varchar (250) not null,
+description text,
+genre varchar(250) not null);
+
+create table journal (
+id serial primary key,
+name varchar(250) not null,
+countPages int not null,
+publicationYear int not null,
+number int not null,
+description text,
+genre varchar(250) not null);
