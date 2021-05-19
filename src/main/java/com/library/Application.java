133914c1afb6c9c61af5bb2c5f4125854fcc28a0
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2021-05-19 21:03:29 +0300

    add DatabaseConnectionManager and PropertiesLoader

--- src/main/java/com/library/Application.java
+++ src/main/java/com/library/Application.java
@@ -1,14 +1,23 @@
 package com.library;
 
 
+import com.library.config.DatabaseConnectionManager;
 import com.library.controller.MainController;
 import com.library.model.DataStorage;
 import com.library.model.InMemoryDataStorage;
+import com.library.util.PropertiesLoader;
 import com.library.view.Console;
 import com.library.view.View;
 
 public class Application {
     public static void main(String[] args) {
+        PropertiesLoader propertiesLoader = new PropertiesLoader();
+        propertiesLoader.loadPropertiesFile("application.properties");
+
+        DatabaseConnectionManager ds = new DatabaseConnectionManager(propertiesLoader.getProperty("host"),
+                propertiesLoader.getProperty("database.name"),
+                propertiesLoader.getProperty("username"),
+                propertiesLoader.getProperty("password"));
         DataStorage storage = new InMemoryDataStorage();
         View view = new Console();
 
---
+++ src/main/resources/ddl.sql
@@ -0,0 +1,1 @@
+create database library_go_it with owner postgres;
