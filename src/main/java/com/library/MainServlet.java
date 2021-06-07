7bc046bc82aa96ca193a1cfe4b3d33db8333af94
Oleksandr Yanov <oleksandr.yanov@thomsonreuters.com>
2021-06-07 21:31:11 +0300

    Add jstl support with example

--- src/main/java/com/library/MainServlet.java
+++ src/main/java/com/library/MainServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @WebServlet("/")
@@ -13,6 +15,13 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<String> names = new ArrayList<>();
+        names.add("Denis");
+        names.add("Hanna");
+        names.add("Vadym");
+        names.add("Oleksandr");
+        names.add("Tetiana");
+        req.setAttribute("students", names);
         req.getRequestDispatcher("/view/index.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/view/index.jsp
+++ src/main/webapp/view/index.jsp
@@ -1,3 +1,4 @@
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,5 +6,9 @@
     </head>
     <body>
         Hello Library GOIT
+
+        <c:forEach var="student" items="${students}">
+            <p>${student}</p>
+        </c:forEach>
     </body>
 </html>
